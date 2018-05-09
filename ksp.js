ca9b711dc188b5fe51a19f6b20b6cd7aa50f845c
DrDam <DrDam8584@gmail.com>
2018-05-09 18:52:52 +0200

    plan + changelog

--- ksp.js
+++ ksp.js
@@ -1,60 +1,58 @@
 // Jquery
-$(function() {
+$(function () {
 
-    // variable des workers
+    //  Workers configuration
     var NbWorkers = 4;
     var Workers = [];
-    var WokerCreated = false;
 
-    // Déclaration des fonctions
+    // Binding Stop button
+    $('#stop').click(function () {
+        for (var i in Workers) {
+            Workers[i].postMessage({channel: "stop"});
+        }
+    });
 
-    
-    $('#stop').click(function() {
-		for(var i in Workers) {
-			Workers[i].postMessage({channel:"stop"});
-		}
-	}) 
-	
-    
-    $('#param').submit(function(event) {
+    // Binding start Button
+    $('#param').submit(function (event) {
         event.preventDefault();
 
-		// récupération des valeurs
-		var elems = event.currentTarget.elements;
+        // Get form values
+        var elems = event.currentTarget.elements;
 
-		var CU = {};
-		CU.mass = elems.Mu.value;
-		CU.taille = elems.tailleCU.value;
+        var CU = {};
+        CU.mass = elems.Mu.value;
+        //CU.taille = elems.tailleCU.value;
 
-		var rocket = {}
-		rocket.dv = elems.DvCible.value;
-		rocket.type = elems.type.value;
-		rocket.desorbit = elems.retour.value;
-		rocket.twr = [elems.Tmin.value, elems.Tmax.value];
-		console.log(rocket);
+        var rocket = {};
+        rocket.dv = elems.DvCible.value;
+        //rocket.type = elems.type.value;
+        //rocket.desorbit = elems.retour.value;
+        rocket.twr = [elems.Tmin.value, elems.Tmax.value];
 
-		// create workers
-		if(WokerCreated == false) {
-			var i = 0;
-			while(i < NbWorkers) {
-				w = new Worker("worker.js");
-				Workers.push(w);
-				i++
-			}
-			WorkerCreated = true;
-		}		
-		
-		// Run workers
-		for(var i in Workers) {
-			Workers[i].postMessage({channel:"init", id:i, start:10*i});
-			Workers[i].postMessage({channel:"run"});
-			Workers[i].onmessage = function(e) {
-				document.getElementById("resultcount" + e.data.id).innerHTML = e.data.result;
-			}
-		}
+        // create workers
+        if (Workers.length == 0) {
+            var i = 0;
+            while (i < NbWorkers) {
+                w = new Worker("worker.js");
+                Workers.push(w);
+                i++;
+            }
+        }
 
-
+        // Run workers
+        for (var i in Workers) {
+            Workers[i].postMessage({channel: "init", id: i, start: 10 * i});
+            Workers[i].postMessage({channel: "run"});
+            Workers[i].onmessage = function (e) {
+                updateDom(e.data);
+            };
+        }
         return false;
     });
+    
+    // Worker effects
+    function updateDom(data) {
+        $("#resultcount" + data.id).html(data.result);
+    }
 
 });
--- worker.js
+++ worker.js
@@ -1,20 +1,7 @@
 var PARAMS = {};
 var result = null;
 
-function addOne(i) {
-    return i + 1;
-}
-
-function run() {
-	if(result == null) {
-		result = PARAMS.base;
-	}
-	result = addOne(result);
-	//console.log('woker '+PARAMS.id+' send to main : ' + result);
-	postMessage({result:result, id:PARAMS.id});
-	PARAMS.timer = setTimeout(run,100);
-}
-
+// Communication
 onmessage = function(e) {
 	if(e.data.channel == 'stop') {
 		clearTimeout(PARAMS.timer);
@@ -27,14 +14,28 @@ onmessage = function(e) {
 	if(e.data.channel == 'init') {
 		PARAMS.id = e.data.id;
 		PARAMS.base = e.data.start;
+                console.log('woker '+PARAMS.id+' initialized');
 		return;
 	}
 	if(e.data.channel == 'run')
 	{
-		//console.log('woker '+PARAMS.id+' initialized with : ' + PARAMS.base);
+		console.log('start woker '+PARAMS.id);
 		run();
 	}
 	
 }
 
+// Core function
+function run() {
+	if(result == null) {
+		result = PARAMS.base;
+	}
+	result = addOne(result);
+	postMessage({result:result, id:PARAMS.id});
+	PARAMS.timer = setTimeout(run,100);
+}
 
+// Processing functions
+function addOne(i) {
+    return i + 1;
+}
