6bb6a0bfaf51a6dcabf19ec7f63b651800e6d913
DrDam <DrDam8584@gmail.com>
2018-05-09 22:47:36 +0200

    add tabledata

--- ksp.js
+++ ksp.js
@@ -4,6 +4,7 @@ $(function () {
     //  Workers configuration
     var NbWorkers = 4;
     var Workers = [];
+    var resultTable = null;
 
     // Binding Stop button
     $('#stop').click(function () {
@@ -14,6 +15,16 @@ $(function () {
 
     // Binding start Button
     $('#param').submit(function (event) {
+
+        if (resultTable === null) {
+            $('#results').show();
+            resultTable = $('#results').DataTable({
+                paging: false,
+                searching: false
+            });
+        }
+
+
         event.preventDefault();
 
         // Get form values
@@ -29,8 +40,13 @@ $(function () {
         //rocket.desorbit = elems.retour.value;
         rocket.twr = [elems.Tmin.value, elems.Tmax.value];
 
+        var data = {
+            rocket: rocket,
+            cu: CU
+        };
+
         // create workers
-        if (Workers.length == 0) {
+        if (Workers.length === 0) {
             var i = 0;
             while (i < NbWorkers) {
                 w = new Worker("worker.js");
@@ -40,8 +56,12 @@ $(function () {
         }
 
         // Run workers
+        var fragment_length = Math.ceil(Engines.length / NbWorkers);
         for (var i in Workers) {
-            Workers[i].postMessage({channel: "init", id: i, start: 10 * i});
+            var start = i * fragment_length;
+            var localengines = Engines.slice(start, start + fragment_length);
+            data.engines = localengines;
+            Workers[i].postMessage({channel: "init", id: i, data: data});
             Workers[i].postMessage({channel: "run"});
             Workers[i].onmessage = function (e) {
                 updateDom(e.data);
@@ -49,10 +69,10 @@ $(function () {
         }
         return false;
     });
-    
-    // Worker effects
+
+    // Add a row in table
     function updateDom(data) {
-        $("#resultcount" + data.id).html(data.result);
+        resultTable.row.add([data.id, data.output]).draw();
     }
 
 });
--- worker.js
+++ worker.js
@@ -1,41 +1,40 @@
-var PARAMS = {};
-var result = null;
+var id;
+var data = {}
+var timer;
 
 // Communication
 onmessage = function(e) {
 	if(e.data.channel == 'stop') {
 		clearTimeout(PARAMS.timer);
                 result = null
-		PARAMS.timer = undefined;
+		timer = undefined;
 		console.log('woker '+PARAMS.id+' stop');
 		return;
 	}
 	
 	if(e.data.channel == 'init') {
-		PARAMS.id = e.data.id;
-		PARAMS.base = e.data.start;
-                console.log('woker '+PARAMS.id+' initialized');
+		id = e.data.id;
+		data = e.data.data;
+                console.log('woker '+id+' initialized');
 		return;
 	}
 	if(e.data.channel == 'run')
 	{
-		console.log('start woker '+PARAMS.id);
-		run();
+		console.log('start woker '+id);
+                console.log(data.engines);
+		//run();
 	}
 	
 }
 
 // Core function
 function run() {
-	if(result == null) {
-		result = PARAMS.base;
-	}
-	result = addOne(result);
-	postMessage({result:result, id:PARAMS.id});
-	PARAMS.timer = setTimeout(run,100);
+	output = drawMeARocket(data);
+	postMessage({output:output, id:id});
+	timer = setTimeout(run,100);
 }
 
 // Processing functions
-function addOne(i) {
-    return i + 1;
+function drawMeARocket(data) {
+
 }
